c0aa0effb3684547cad39fc06c8436f12f036d88
Arslan Arshad <ArslanArshad07@live.com>
2018-07-18 00:47:11 +0500

    echo configured

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -50,7 +50,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: b8c33283da2abe3c63e3,
+    cluster: mt1,
     encrypted: true
 });
